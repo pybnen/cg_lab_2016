15573e5f9ab79d5908325816bd10366c23c25900
= <pybnen@gmail.com>
2016-06-16 17:29:11 +0200

    Implement exercise 1

--- 01_intro_handout/main.js
+++ 01_intro_handout/main.js
@@ -8,7 +8,35 @@ function init(resources) {
   //create a GL context
   gl = createContext(400 /*width*/, 400 /*height*/);
 
-  //TODO initialize shader, buffers, ...
+  //in WebGL / OpenGL3 we have to create and use our own shaders for the programmable pipeline
+  //create the shader program
+  shaderProgram = createProgram(gl, resources.vs, resources.fs);
+
+  // Create a buffer and put a single clipspace rectangle in
+  // it (2 triangles)
+  buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  //we need typed arrays
+  const arr = new Float32Array([
+    -1.0, -1.0,
+    1.0, -1.0,
+    -1.0, 1.0,
+    -1.0, 1.0,
+    1.0, -1.0,
+    1.0, 1.0]);
+  //copy data to GPU
+  gl.bufferData(gl.ARRAY_BUFFER, arr, gl.STATIC_DRAW);
+  //same for the color
+  colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  const colors = new Float32Array([
+    1, 0, 0, 1,
+    0, 1, 0, 1,
+    0, 0, 1, 1,
+    0, 0, 1, 1,
+    0, 1, 0, 1,
+    0, 0, 0, 1]);
+  gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
 }
 
 /**
@@ -20,14 +48,27 @@ function render() {
   //clear the buffer
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  //TODO render scene
+  gl.useProgram(shaderProgram);
 
-  //request another call as soon as possible
-  //requestAnimationFrame(render);
+  var userColor = { r: 0.6, g: 0.2, b: 0.8 };
+  gl.uniform3f(gl.getUniformLocation(shaderProgram, 'u_usercolor'),
+    userColor.r, userColor.g, userColor.b);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.enableVertexAttribArray(gl.getAttribLocation(shaderProgram,'a_position'));
+  gl.vertexAttribPointer(gl.getAttribLocation(shaderProgram,'a_position'), 2 /*per vertex*/, gl.FLOAT, false, 0, 0);
+
+  const colorLocation = gl.getAttribLocation(shaderProgram, 'a_color');
+  gl.enableVertexAttribArray(colorLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.vertexAttribPointer(colorLocation, 4, gl.FLOAT, false, 0, 0);
+
+  gl.drawArrays(gl.TRIANGLES, 0, 6 /*count*/);
 }
 
 loadResources({
-  //list of all resources that should be loaded as key: path
+  vs: 'shader/simple.vs.glsl',
+  fs: 'shader/simple.fs.glsl'
 }).then(function (resources /*loaded resources*/) {
   init(resources);
   //render one frame
